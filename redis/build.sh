00dc75df776b1cf1bd238f5f815a307f8ef5dbdb
Lee Benson <lee.benson@imperial.ac.uk>
2024-10-02 17:17:01 +0100

    redis server

--- redis/build.sh
+++ redis/build.sh
@@ -5,16 +5,17 @@ echo $'\n'"lxc.mount.entry = ${PWD}/mount home/host none bind,create=dir 0 0" \
     | cat ${CONFIG_ROOT} -\
     | tee -a ${NAME}.conf
 
-source ../create_container.sh 
+source ../create_container.sh
+source ../secrets.sh
 
 create_container ${NAME} ${NAME}.conf && \
     #lxc will store the container config somewhere
     rm -f ${NAME}.conf && \ 
-    systemd-run --user --scope -p "Delegate=yes" -- lxc-start -n ${FULL_NAME} && \
-    systemd-run --user --scope -p "Delegate=yes" -- lxc-attach -n ${FULL_NAME} --clear-env \
-		-- /home/host/scripts/redis_build.sh
-    systemd-run --user --scope -p "Delegate=yes" -- lxc-attach -n ${FULL_NAME} --clear-env \
-		-- /home/host/scripts/redis_configure.sh
+    systemd-run --user --scope -p "Delegate=yes" -- lxc-start -n ${NAME} && \
+    systemd-run --user --scope -p "Delegate=yes" -- lxc-attach -n ${NAME} --clear-env \
+		-- /home/host/scripts/build.sh
+    systemd-run --user --scope -p "Delegate=yes" -- lxc-attach -n ${NAME} --clear-env \
+		-- /home/host/scripts/configure.sh
     
 
 
