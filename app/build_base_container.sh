284f1135797d85e33dc5f44bebb7134a883f97bd
Lee Benson <lee.benson@imperial.ac.uk>
2024-10-16 14:41:51 +0100

    File system logging enabled in app

--- app/build_base_container.sh
+++ app/build_base_container.sh
@@ -1,6 +1,7 @@
 NAME=$1 #ae-datastore
 CONFIG_ROOT=$2 #../lxc_config.conf
 DATA_MNT=$3 #/home/leeb/.local/var/lxc/ae-datastore/data
+LOG_MNT=$4 #/home/leeb/.local/var/lxc/ae-datastore/log
 
 source ../create_container.sh
 source ../secrets.sh
@@ -26,6 +27,7 @@ NODE_SUFFIX= # Default is linux-x64.tar.xz, for something else, e.g. export NODE
 
 echo $'\n'"lxc.mount.entry = ${PWD}/mount home/host none bind,create=dir 0 0"\
      $'\n'"lxc.mount.entry = ${DATA_MNT} opt/invenio/var/instance/data none bind,create=dir 0 0"\
+     $'\n'"lxc.mount.entry = ${LOG_MNT} opt/invenio/var/instance/log none bind,create=dir 0 0"\
     | cat ${CONFIG_ROOT} -\
     | tee -a ${NAME}.conf
 
